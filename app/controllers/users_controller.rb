2168e59a27debd307ae915739de6310333087726
Dang Van <dinvvan@gmail.com>
2018-10-17 00:19:29 +0700

    finish login-logout basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:danger] = "Welcome to Imusic"
       redirect_to @user
     else
